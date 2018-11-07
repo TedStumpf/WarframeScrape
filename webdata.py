a49bfac3a2bc1d9a09e9c2a0f370f2e90939c697
Theodore Stumpf <theodorestumpf@gmail.com>
2018-11-07 17:49:06 -0500

    Added initial web scrape

--- webdata.py
+++ webdata.py
@@ -1,5 +1,20 @@
 #   webdata.py
 #   Requests the data from the Warframe wiki
+import requests
+from pprint import pprint
+from bs4 import BeautifulSoup
+
+#   The base url for the warframe wikia
+wiki_url = 'https://warframe.fandom.com'
+
+#   get_page
+#   Returns a BeautifulSoup object for the given page
+#   url:    The URL of the target page
+def get_page(url):
+   req = requests.get(url)
+   if (req.status_code != 200):
+      return None
+   return BeautifulSoup(req.content, 'html.parser')
 
 #   data_saved
 #   Returns True if there is a local copy of the data
@@ -10,7 +25,24 @@ def data_saved():
 #   Returns a dictionary of data
 #   forced_refresh: If true, this forces a refresh from the wiki
 def get_data(forced_refresh = False):
-    pass
+    data = {}
+    weapon_pages_url = {
+        'primary':'/wiki/Category:Primary_Weapons', 
+        'melee':'/wiki/Category:Melee_Weapons'
+    }
+    
+    for key, url in weapon_pages_url.items():
+        weapon_page = get_page(wiki_url + url)
+        weapon_list = weapon_page.find('div', {'class': 'category-page__members'})
+        weapon_entries = weapon_list.find_all('a', {'class': 'category-page__member-link'})
+        data[key] = {wep.attrs['title']:{'title': wep.attrs['title'], 'link': wep.attrs['href']} 
+            for wep in weapon_entries
+            if not 'Conclave' in wep.attrs['title']
+            and not 'Category:' in wep.attrs['title']
+            and not 'File:' in wep.attrs['title']
+            and not 'PvP' in wep.attrs['title']}
+    pprint(data)
+        
 
 #   save_data
 #   Saves the data to a local file
