10fc6f1dcde2ab0edb54e18e5b2f20c8cd56026a
Theodore Stumpf <theodorestumpf@gmail.com>
2018-11-07 11:03:14 -0500

    Added skeleton functions for webdata.py

--- webdata.py
+++ webdata.py
@@ -1,2 +1,23 @@
 #   webdata.py
 #   Requests the data from the Warframe wiki
+
+#   data_saved
+#   Returns True if there is a local copy of the data
+def data_saved():
+    pass
+
+#   get_data
+#   Returns a dictionary of data
+#   forced_refresh: If true, this forces a refresh from the wiki
+def get_data(forced_refresh = False):
+    pass
+
+#   save_data
+#   Saves the data to a local file
+def save_data(data):
+    pass
+
+#   load_data
+#   Loads the data from a local file
+def load_data():
+    pass
