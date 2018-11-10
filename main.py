ea50123e9e11e798c6e57a3e487412f0df944977
Unknown <theodorestumpf@gmail.com>
2018-11-09 19:07:19 -0500

    Added initial Kivy implementation

--- main.py
+++ main.py
@@ -4,58 +4,52 @@
 from webdata import *
 from random import randrange
 from pprint import pprint
+#   Kivy
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.recycleview import RecycleView
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.image import Image
 
-def main():
-    data = get_data(True)
-    #test01(data)
-    #test02(data)
-    #test03(data)
-    test04(data)
-    save_data(data)
-    print("Done")
-    
+class MenuScreen(Screen):
+    def __init__(self, **kwargs):
+        super(MenuScreen, self).__init__(**kwargs)
+        self.name = 'screen_main'
 
+        self.base_layout = FloatLayout()
+        self.add_widget(self.base_layout)
+        scroll = RecycleView(size = (400, 400), size_hint = (None, None), center = Window.center)
+        scroll.background = (1, 1, 1)
+        self.base_layout.add_widget(scroll)
+        scroll_box = BoxLayout(orientation='vertical', spacing = 2, padding = 10, size_hint_y = None)
+        scroll_box.bind(minimum_height=scroll_box.setter('height'))
+        scroll.add_widget(scroll_box)
 
-def test01(data):
-    pv = data['Prova Vandal']
-    expand_data(pv)
-    print(pv['name'])
-    pprint(pv)
-    df = data['Dex Furis']
-    expand_data(df)
-    print(df['name'])
-    pprint(df)
+        self.load_data()
 
-def test02(data):
-    detail = [randrange(len(data)) for i in range(5)]
-    for d in detail:
-        entry = list(data.items())[d]
-        print('\n', entry[0])
-        expand_data(entry[1])
-        pprint(entry[1])
-
-def test03(data):
-    i = 0
-    for key, val in data.items():
-        print(key, end = "   ")
-        expand_data(val)
-        i += 1
-        print(i, "/", len(data))
-
-def test04(data):
-    run = True
-    while(run):
-        inp = input("Enter a weapon name: ").lower()
-        if inp == "quit":
-            run = False
-        else:
-            if inp in data.keys():
-                entry = data[inp]
-                expand_data(entry)
-                pprint(entry)
-            else:
-                print("Weapon not found")
+        for key, item in list(self.data.items()) :
+            l = Button(text = item['name'], size = (400, 30), size_hint = (1, None))
+            scroll_box.add_widget(l)
 
 
+    def load_data(self):
+        self.data = get_data()
+        save_data(self.data)
+        print('Data Loaded')
 
-main()
+
+class Main(App):
+    def build(self):
+        menu = MenuScreen()
+        self.screenmanager = ScreenManager()
+        self.screenmanager.add_widget(menu)
+        self.screenmanager.current = menu.name
+        return self.screenmanager
+
+if (__name__ == '__main__'):
+    Main().run()
